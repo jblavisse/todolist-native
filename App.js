2c129678572e3f1a8868731763076232da72496d
JB Lavisse <contact@jlavisse.com>
2020-07-06 16:30:59 +0200

    Add tasks list with delete icon

--- App.js
+++ App.js
@@ -1,14 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; 
+
+class App extends Component {
+  state = {
+    tasks: [
+      {id: 1, title: "Faire l'exo météo"},
+      {id: 2, title: "Faire l'exo MERISE"}
+    ]
+  }
+
+  hello() {
+    console.log("coucou");
+  }
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.tasks}
+          renderItem={({item}) => 
+            <View style={styles.task}> 
+              <Text style={styles.taskTitle}>{item.title}</Text>
+              <TouchableOpacity onPress={this.hello}>
+                <MaterialIcons name="delete" size={32} color="white" />
+              </TouchableOpacity>
+            </View>}
+          keyExtrator={item => item.id.toString()}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +41,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  task: {
+    padding: 20,
+    margin: 5,
+    width: 300,
+    backgroundColor: 'lightseagreen',
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  taskTitle: {
+    color: 'white',
+    fontSize: 20
+  }
+})
+
+
+export default App;
